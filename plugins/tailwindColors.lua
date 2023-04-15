24709a0b79793cb774b0618b8bec8d551d204a10
Michael Carno <michaelcarno@mail.ru>
2023-04-16 04:32:12 +0700

    Added tailwindcolors plugin

---
+++ plugins/tailwindColors.lua
@@ -0,0 +1,17 @@
+return {
+  "themaxmarchuk/tailwindcss-colors.nvim",
+  event = "BufRead",
+  config = function()
+    local nvim_lsp = require("lspconfig")
+
+    local on_attach = function(client, bufnr)
+      -- other stuff --
+      require("tailwindcss-colors").buf_attach(bufnr)
+    end
+
+    nvim_lsp["tailwindcss"].setup({
+      -- other settings --
+      on_attach = on_attach,
+    })
+  end
+}
